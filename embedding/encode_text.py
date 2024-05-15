5f9f460107ee95805989591a951977f14a8ea9e0
dangnguyenngochai <68412631+dangnguyenngochai@users.noreply.github.com>
2024-05-15 22:39:55 +0700

    Update encode_text.py

--- embedding/encode_text.py
+++ embedding/encode_text.py
@@ -8,34 +8,44 @@ from transformers import AutoModel, AutoTokenizer
 
 from qdrant_client import QdrantClient
 
-test_transcription_path = '../data/transcription/cX4DUogRjso.csv'
-emd_model = "Alibaba-NLP/gte-large-en-v1.5"
+class EmcodedTranscriptpionVectorStore:
+    def __init__(self, emd_model_name, collection_name):
+        self.collection_name = collection_name
+        self.qdrant_local_path = "/tmp/local_qdrant"
+        self.qdrant_client = QdrantClient()
+        self.emd_model = HuggingFaceEmbeddings(
+            model_name=emd_model_name,
+            model_kwargs={
+                'trust_remote_code': True
+            }
+        )
+        self.vector_store = Qdrant(
+            client=self.qdrant_client, collection_name=self.collection_name, 
+            embeddings=self.emd_model,
+        )
 
+    def embeddings_transcription(self, transcription_data_path: str, collection_name: str, db: QdrantClient):
+        try:
+            raw_documents = TextLoader(transcription_data_path).load()
+            text_splitter = CharacterTextSplitter(
+                separator="\n\n",
+                is_separator_regex=False,
+            )
+            documents = text_splitter.split_documents(raw_documents)
+            _ = self.vector_store.add_documents(documents)
 
-def embeddings_data(transcription_data_path):
+        except Exception as ex:
+            print(ex)
 
-    raw_documents = TextLoader(transcription_data_path).load()
-    text_splitter = CharacterTextSplitter(
-        separator="\n\n",
-        is_separator_regex=False,
-    )
-    documents = text_splitter.split_documents(raw_documents)
-
-    embeddings = HuggingFaceEmbeddings(
-        model_name=emd_model,
-        model_kwargs={
-            'trust_remote_code': True
-        }
-    )
-
-    db = Qdrant.from_documents(documents, embeddings, location=":memory:", collection_name="my_documents")
-    return db
-    
 def test_query():
     pass
     
 if __name__ == "__main__":
-    query = "pirate on sony"
-    docs = db.similarity_search(query)
-    embeddings_data = 
+    test_transcription_path = '../data/state_of_union.csv'
+    test_query = "covid-19"
+    model_name = "Alibaba-NLP/gte-large-en-v1.5"
+    collection_name = 'transcription_state_of_union'
+
+    vstore = EmcodedTranscriptpionVectorStore(emd_model_name=model_name, collection_name=collection_name)
+    docs = vstore.vector_store.similarity_search(test_query)
     print(docs[0].page_content)
