c476489ef999c72bd79a87037784603d206fda79
dangnguyenngochai <68412631+dangnguyenngochai@users.noreply.github.com>
2024-05-15 23:48:25 +0700

    Update encode_text.py

--- embedding/encode_text.py
+++ embedding/encode_text.py
@@ -11,20 +11,27 @@ from qdrant_client import QdrantClient
 class EmcodedTranscriptpionVectorStore:
     def __init__(self, emd_model_name, collection_name):
         self.collection_name = collection_name
-        self.qdrant_local_path = "/tmp/local_qdrant"
-        self.qdrant_client = QdrantClient()
+        self.qdrant_local_path = "local_qdrant"
         self.emd_model = HuggingFaceEmbeddings(
             model_name=emd_model_name,
             model_kwargs={
                 'trust_remote_code': True
             }
         )
-        self.vector_store = Qdrant(
-            client=self.qdrant_client, collection_name=self.collection_name, 
-            embeddings=self.emd_model,
-        )
+        if not qdrant_client.collection_exists(self.collection_name):
+            self.vector_store = Qdrant(
+                client=QdrantClient(path=qdrant_local_path), 
+                collection_name=self.collection_name, 
+                embeddings=self.emd_model,
+            )
+        else:
+            self.vector_store = Qdrant.from_existing_collection( 
+                local=self.qdrant_local_path,
+                collection_name=self.collection_name, 
+                embeddings=self.emd_model,
+            )
 
-    def embeddings_transcription(self, transcription_data_path: str, collection_name: str, db: QdrantClient):
+    def embeddings_transcription(self, transcription_data_path: str, collection_name: str):
         try:
             raw_documents = TextLoader(transcription_data_path).load()
             text_splitter = CharacterTextSplitter(
@@ -33,6 +40,12 @@ class EmcodedTranscriptpionVectorStore:
             )
             documents = text_splitter.split_documents(raw_documents)
             _ = self.vector_store.add_documents(documents)
+            self.vector_store = Qdrant.from_documents(
+                documents,
+                self.emd_model,
+                path=self.qdrant_local_path,
+                collection_name=collection_name
+            )
 
         except Exception as ex:
             print(ex)
@@ -41,11 +54,12 @@ def test_query():
     pass
     
 if __name__ == "__main__":
-    test_transcription_path = '../data/state_of_union.csv'
+    test_transcription_path = '../state_of_union.txt'
     test_query = "covid-19"
     model_name = "Alibaba-NLP/gte-large-en-v1.5"
     collection_name = 'transcription_state_of_union'
 
     vstore = EmcodedTranscriptpionVectorStore(emd_model_name=model_name, collection_name=collection_name)
-    docs = vstore.vector_store.similarity_search(test_query)
-    print(docs[0].page_content)
+    vstore.embeddings_transcription(test_transcription_path, collection_name)
+    # docs = vstore.vector_store.similarity_search(test_query)
+    # print(docs[0].page_content)
